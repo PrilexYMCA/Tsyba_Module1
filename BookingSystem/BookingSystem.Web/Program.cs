5af80d8279dd0d2ce49db5cf9fc76752a3c31479
PrilexYMCA <mixacik4002@gmail.com>
2025-06-04 14:08:56 +0300

    first commit

---
+++ BookingSystem/BookingSystem.Web/Program.cs
@@ -0,0 +1,32 @@
+// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using BookingSystem.Data;
+using BookingSystem.Web.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDocumentation();
+builder.Services.AddDataAccess(builder.Configuration);
+builder.Services.AddDomainServices();
+builder.Services.AddAutoMapper();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDocumentation();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+await dbContext.Database.MigrateAsync();
+
+await app.RunAsync();
